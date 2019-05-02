acf5e07eeaaaf5ba10a076a762bf54ba9f352084
CodeQue <khaliqojikutu@gmail.com>
2019-05-02 13:39:58 +0100

    Initial commit

---
+++ src/merchant.php
@@ -0,0 +1,11 @@
+<?php
+//declare file namespace
+namespace VoguePay;
+// declare holding array details for merchant details
+
+class merchantConfiguration {
+   function api () { return 'https://voguepay.com/api/'; }
+   function card () { return 'card'; }
+   function getResponse () { return 'query'; }
+}
+?>
